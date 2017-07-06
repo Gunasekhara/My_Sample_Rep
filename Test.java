38a97f7a3c7da50cb49f97b7f607b24358309e82
guna <guna.2009sekhar@gmail.com>
2017-07-06 13:58:26 +0530

    variable is added to the Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,10 @@ package evry.Calculator;
 public class Test 
 {
     public static void main( String[] args )
-    {
+    {  
+	int x=10;
         System.out.println( "Hello World!" );
+	 System.out.println( "x value is ="+x );
+
     }
 }
